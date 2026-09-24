df18165331b15a2755899a56483d1e74d41edc8b
Slava Chan <49863417+UyNewNas@users.noreply.github.com>
2026-09-24 22:41:00 +0800

    chore: import fixed-cutoff Cesaro limit module

--- OmegaBalance.lean
+++ OmegaBalance.lean
@@ -18,5 +18,6 @@ import OmegaBalance.F3CorrelationDepth
 import OmegaBalance.F3CorrelationClosed
 import OmegaBalance.F3CorrelationLimit
 import OmegaBalance.F3CorrelationCesaro
+import OmegaBalance.F3CorrelationCesaroLimit
 import OmegaBalance.F3Infinitude
 import OmegaBalance.F3SignChanges
